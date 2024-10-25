fe22e7af3884fe7264a75745de69962a616b8807
Zixiang Hu <144851646+ZackHu-2001@users.noreply.github.com>
2024-10-24 21:50:27 -0700

    feat: set up GA

---
+++ firebase/setup.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAZqHTg5QDJlTEAP3bVdQOM9oy-8zfLQII",
+    authDomain: "game-based-learning-3a957.firebaseapp.com",
+    projectId: "game-based-learning-3a957",
+    storageBucket: "game-based-learning-3a957.appspot.com",
+    messagingSenderId: "226620441583",
+    appId: "1:226620441583:web:6400b6d60dfbf72b212c1a",
+    measurementId: "G-2142X6GY7M"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import ReactGA from 'react-ga4'
+
+ReactGA.initialize('G-2142X6GY7M'); // Replace with your tracking ID
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
