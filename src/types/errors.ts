592a6317efb299f306ae58edcb15a835f5a57083
Zixiang Hu <144851646+ZackHu-2001@users.noreply.github.com>
2024-11-25 21:32:15 -0800

    feat: update prompt message and error handling

--- src/types/errors.ts
+++ src/types/errors.ts
@@ -11,26 +11,27 @@ export class GameError extends Error {
 }
 
 export const GameErrorCodes = {
-    INVALID_MOVE: 'INVALID_MOVE',
-    WORKER_BUSY: 'WORKER_BUSY',
-    SLOT_OCCUPIED: 'SLOT_OCCUPIED',
-    SLOT_EMPTY: 'SLOT_EMPTY',
-    INPUT_EMPTY: 'INPUT_EMPTY',
+    EMPTY_HAND_OUTPUT: 'EMPTY_HAND_OUTPUT',
+    COPYFROM_EMPTY: 'COPYFROM_EMPTY',
+    EMPTY_HAND_COPYTO: 'EMPTY_HAND_COPYTO',
+    EMPTY_HAND_ADD: 'EMPTY_HAND_ADD',
+    EMPTY_HAND_SUB: 'EMPTY_HAND_SUB',
+    EMPTY_HAND_JUMP_ZERO: 'EMPTY_HAND_JUMP_ZERO',
+    EMPTY_HAND_JUMP_NEG: 'EMPTY_HAND_JUMP_NEG',
     ADD_EMPTY: 'ADD_EMPTY',
     SUB_EMPTY: 'SUB_EMPTY',
-    WORKER_NOT_CARRYING: 'WORKER_NOT_CARRYING',
-    INVALID_OPERATION: 'INVALID_OPERATION',
 } as const;
 
 
 export const ErrorMessages = {
-    [GameErrorCodes.INVALID_MOVE]: '无效的移动',
-    [GameErrorCodes.WORKER_BUSY]: '工人正在忙碌',
-    [GameErrorCodes.SLOT_OCCUPIED]: '该位置已被占用',
-    [GameErrorCodes.SLOT_EMPTY]: 'Failed to pick up from an empty slot!',
-    [GameErrorCodes.INPUT_EMPTY]: 'No stone in the input area!',
-    [GameErrorCodes.ADD_EMPTY]: 'No stone in the add area!',
-    [GameErrorCodes.SUB_EMPTY]: 'No stone in the sub area!',
-    [GameErrorCodes.WORKER_NOT_CARRYING]: 'Worker is not carrying stone',
-    [GameErrorCodes.INVALID_OPERATION]: '无效的操作',
+    [GameErrorCodes.EMPTY_HAND_OUTPUT]: 'Empty Value! You can not OUTPUT with empty hands!',
+    [GameErrorCodes.COPYFROM_EMPTY]: 'Empty Value! You can not COPYFROM from an empty slot! Try move something to the slot first!',
+    [GameErrorCodes.EMPTY_HAND_COPYTO]: 'Empty Value! You can not COPYTO with empty hands!',
+    [GameErrorCodes.EMPTY_HAND_ADD]: 'Empty Value! You can not ADD with empty hands!',
+    [GameErrorCodes.EMPTY_HAND_SUB]: 'Empty Value! You can not SUB with empty hands!',
+    [GameErrorCodes.EMPTY_HAND_JUMP_ZERO]: 'Empty Value! You can not JUMP IF ZERO with empty hands!',
+    [GameErrorCodes.EMPTY_HAND_JUMP_NEG]: 'Empty Value! You can not JUMP IF NEGATIVE with empty hands!',
+    [GameErrorCodes.ADD_EMPTY]: 'Empty Value! You can not ADD to an empty slot! Try move something to the slot first!',
+    [GameErrorCodes.SUB_EMPTY]: 'Empty Value! You can not SUB from an empty slot! Try move something to the slot first!',
+
 } as const;
--- src/types/game.ts
+++ src/types/game.ts
@@ -22,7 +22,7 @@ export type outputFn = (nextInt: (min: number, max: number) => number) => number
 export type CommandType =
     'INPUT' | 'OUTPUT' | 'COPYFROM' | 'COPYTO' |
     'ADD' | 'SUB' | 'BUMPUP' | 'BUMPDOWN' |
-    'JUMP' | 'JUMPZ' | 'JUMPN' | 'LABEL' | "";
+    'JUMP' | 'JUMP = 0' | 'JUMP < 0' | 'LABEL' | "";
 
 export type CommandWithArgType = {
     command: CommandType;
