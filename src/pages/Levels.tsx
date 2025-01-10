b384e5251b503b0e9fcd11fc7a973cf6e6404433
Zixiang Hu <144851646+ZackHu-2001@users.noreply.github.com>
2025-01-09 16:18:59 -0800

    feat: map integration

--- src/pages/Levels.tsx
+++ src/pages/Levels.tsx
@@ -7,6 +7,8 @@ import { RxReset } from "react-icons/rx";
 interface LevelCoordinate {
     x: number;
     y: number;
+    w: number;
+    h: number;
 }
 
 type LevelCoordinates = LevelCoordinate[];
@@ -51,27 +53,115 @@ const Levels: React.FC = () => {
 
     const LEVEL_COORDINATES: LevelCoordinates = [
         {
-            x: 0,
-            y: 0,
-        }
+            x: 620,
+            y: 1180,
+            w: 7,
+            h: 5
+        },
+        {
+            x: 700,
+            y: 1050,
+            w: 7,
+            h: 5
+        }, {
+            x: 480,
+            y: 820,
+            w: 6,
+            h: 4
+        }, {
+            x: 570,
+            y: 740,
+            w: 6,
+            h: 4
+        }, {
+            x: 880,
+            y: 770,
+            w: 7,
+            h: 5
+        }, {
+            x: 650,
+            y: 650,
+            w: 5,
+            h: 3.5
+        }, {
+            x: 830,
+            y: 590,
+            w: 5,
+            h: 4
+        }, {
+            x: 660,
+            y: 460,
+            w: 5,
+            h: 4
+        }, {
+            x: 930,
+            y: 440,
+            w: 5,
+            h: 4
+        }, {
+            x: 650,
+            y: 395,
+            w: 5,
+            h: 3
+        }, {
+            x: 815,
+            y: 375,
+            w: 4,
+            h: 3
+        }, {
+            x: 830,
+            y: 235,
+            w: 4,
+            h: 3
+        }, {
+            x: 605,
+            y: 305,
+            w: 4,
+            h: 3
+        }, {
+            x: 700,
+            y: 215,
+            w: 4,
+            h: 3
+        }, {
+            x: 790,
+            y: 150,
+            w: 4,
+            h: 3
+        }, {
+            x: 610,
+            y: 170,
+            w: 4,
+            h: 3
+        }, {
+            x: 620,
+            y: 100,
+            w: 4,
+            h: 3
+        }, {
+            x: 725,
+            y: 100,
+            w: 4,
+            h: 3
+        },
     ]
 
     return currentScene === 'LEVELS' && (
 
         <div className="select-none fixed inset-0 flex flex-row justify-center overflow-scroll" >
-            <img src="./map.webp" className="h-[160vh]" alt="" onClick={() => handleClickLevel(levelsInfo[5])} />
+            <img src="./map.webp" className="h-[160vh]" alt="" />
             {LEVEL_COORDINATES.map((level, idx) => {
                 return (
-                    <div key={idx} className="absolute " style={{ left: `${level.x}px`, top: `${level.y}px` }}>
-                        <button
-                            className={`fixed w-16 h-16 rounded bg-transparent`}
-                            onClick={(e) => {
-                                e.stopPropagation();
-                                handleClickLevel(levelsInfo[idx]);
-                            }}
-                        >
-                        </button>
-                    </div>
+                    <button
+                        key={idx}
+                        className={`absolute bg-transparent border-black rounded-full`}
+                        style={{ left: `${level.x}px`, top: `${level.y}px`, width: `${level.w}rem`, height: `${level.h}rem` }}
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            handleClickLevel(levelsInfo[idx]);
+                        }}
+                    >
+                    </button>
                 )
             })}
             <button
