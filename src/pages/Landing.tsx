003d5175f4d98c83a29bf38efc60405e5f3c189c
Zixiang Hu <144851646+ZackHu-2001@users.noreply.github.com>
2025-02-08 12:29:28 -0800

    feat: update start button

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,11 +1,39 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import GameContext from "../context/GameContext";
 import { useGameStorage } from "../hooks/useStorage/useGameStorage";
 import { ChevronRight } from "lucide-react";
 
+const AnimatedStart = ({ navTo }) => {
+    const [isAnimating, setIsAnimating] = useState(false);
+
+    const handleClick = () => {
+        setIsAnimating(true);
+        setTimeout(() => {
+            navTo('LEVELS');
+        }, 500);
+    };
+
+    return (
+        <div
+            className={`absolute top-2/3 -translate-y-full left-1/2 -translate-x-1/2 pl-4 pr-2 pt-6 pb-4 text-2xl filter backdrop-blur-md rounded-lg cursor-pointer ${isAnimating ? 'w-16' : 'w-32'} transition-[width] duration-500`}
+            onClick={handleClick}
+        >
+            <span className="relative block text-center">
+                <span className={`transition-opacity duration-500 ${isAnimating ? 'opacity-0' : 'opacity-100'}`}>
+                    Start
+                </span>
+                <ChevronRight
+                    className={`w-8 h-8 absolute -translate-y-[2.25rem] left-0 transition-transform duration-500 ${isAnimating ? 'translate-x-0' : '-translate-x-2'}`}
+                />
+            </span>
+        </div>
+    );
+};
+
 const Landing: React.FC = () => {
     const { currentScene, navTo } = useContext(GameContext);
     useGameStorage();
+
 
     return currentScene === 'LANDING' && (
         <>
@@ -20,14 +48,7 @@ const Landing: React.FC = () => {
             <div className="absolute top-1/3 -translate-y-full left-1/2 -translate-x-1/2 px-[2rem] pt-[2rem] text-7xl text-yellow-600 filter backdrop-blur-lg rounded-lg ">
                 Ancient Architect
             </div>
-            <div className="absolute top-2/3 -translate-y-full left-1/2 -translate-x-1/2 pl-[1.5rem] pr-[1rem] pt-[1.5rem] pb-[1rem] text-2xl filter backdrop-blur-md rounded-lg cursor-pointer"
-                onClick={() => {
-                    setTimeout(() => {
-                        navTo('LEVELS');
-                    }, 500);
-                }}>
-                Start <ChevronRight className="inline" />
-            </div>
+            <AnimatedStart navTo={navTo} />
         </>
     );
 }
